6000b61ace94362e4e066a08c1565dcb05785612
Lancel <maurotoscano2@gmail.com>
2020-01-28 19:54:02 -0300

    Fixed a bug where the program used one less core than expected

--- main.c
+++ main.c
@@ -6,9 +6,11 @@
 #include "jacobi.h"
 #include "matrices_aleatorias.h"
 
+const int n = 20000; //La matriz es de nxn
+const int MAX_NUCLEOS = 8;
+
 int main(){
   srand(time(NULL));
-  int n = 20000;
   double** matriz = malloc(n*sizeof(double *));
   for(int i=0; i< n; i++) matriz[i] = malloc(n*sizeof(double));
   double* b = malloc(n*sizeof(double));
@@ -17,12 +19,11 @@ int main(){
   alocar_un_vector_aleatorio(b, n);
   alocar_una_matriz_diagonal_dominante_aleatoria(matriz, n);
 
-  const int MAX_NUCLEOS=8;
   double tiempo_usado[MAX_NUCLEOS];
   double start_t, end_t;
   for(int i = 0; i < MAX_NUCLEOS; i++){
     printf("Comienzo usando %i nucleos\n", i+1);
-    omp_set_num_threads(i);
+    omp_set_num_threads(i+1);
     start_t = omp_get_wtime();
     resolver_sistema_con_jacobi(matriz,n,b,solucion);
     end_t = omp_get_wtime();
